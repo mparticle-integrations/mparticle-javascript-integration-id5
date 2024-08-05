18ba10aec67b2dceabd9daa3e51d7d10fa75805e
SbDove <100377552+SbDove@users.noreply.github.com>
2024-08-05 11:45:34 -0700

    Initial kit release

---
+++ test/end-to-end-testapp/settings.js
@@ -0,0 +1,10 @@
+var SDKsettings = {
+    partnerId: 1234
+    /* fill in SDKsettings with any particular settings or options your sdk requires in order to
+    initialize, this may be apiKey, projectId, primaryCustomerType, etc. These are passed
+    into the src/initialization.js file as the
+    */
+};
+
+// Do not edit below:
+module.exports = SDKsettings;
