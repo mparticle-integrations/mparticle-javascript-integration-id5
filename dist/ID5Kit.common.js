4f0837e3423ab266f5a3ce73518c3360a9cd35ca
Alexander Sapountzis <alexs@mparticle.com>
2024-11-06 15:30:42 -0500

    chore(build): Generate latest bundle [skip ci]

--- dist/ID5Kit.common.js
+++ dist/ID5Kit.common.js
@@ -1028,16 +1028,16 @@ Common.prototype.exampleMethod = function () {
 };
 
 Common.prototype.logId5Id = function (id5Id) {
-    //Checks and saves ID5 ID if it is new
-    if (id5Id !== this.id5Id) {
-        this.id5Id = id5Id;
-        this.id5IdSent = false;
+    if (!id5Id) {
+        return;
     }
 
-    //Sets user attribute if ID is unsent.
-    //This function will be updated once the decryption architecture is finalized.
-    //TO-DO: Log the ID5 ID to correct location
+    var integrationAttributes = {
+        encryptedId5Id: id5Id,
+        id5IdType: this.id5IdType,
+    };
 
+    window.mParticle.setIntegrationAttributes(this.moduleId, integrationAttributes);
 };
 
 Common.prototype.buildPartnerData = function (mParticleUser) {
@@ -1307,6 +1307,9 @@ var initialization = {
 */
     initForwarder: function(forwarderSettings, testMode, userAttributes, userIdentities, processEvent, eventQueue, isInitialized, common) {
         /* `forwarderSettings` contains your SDK specific settings such as apiKey that your customer needs in order to initialize your SDK properly */
+        common.partnerId = forwarderSettings.partnerId;
+        common.id5IdType = forwarderSettings.id5IdType;
+        common.moduleId = this.moduleId;
 
         if (!testMode) {
             /* Load your Web SDK here using a variant of your snippet from your readme that your customers would generally put into their <head> tags
@@ -1316,10 +1319,6 @@ var initialization = {
             var id5Script = document.createElement('script');
             id5Script.src = 'https://cdn.id5-sync.com/api/1.0/id5-api.js';
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(id5Script);
-
-            common.id5Id = null;
-            common.id5IdSent = false;
-            common.partnerId = forwarderSettings.partnerId;
 
             id5Script.onload = function() {
 
